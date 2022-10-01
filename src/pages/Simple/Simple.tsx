41831e079eef5ae32158afd4787766ce48684bd6
JohnRamberger <jramberger3@gatech.edu>
2022-10-01 18:21:47 -0400

    Update Simple.tsx
    added page styling

--- src/pages/Simple/Simple.tsx
+++ src/pages/Simple/Simple.tsx
@@ -1,10 +1,12 @@
-import React, { FC } from 'react';
-import styles from './Simple.module.scss';
+import React, { FC } from "react";
+import styles from "./Simple.module.scss";
+
+import page_styles from "../page.module.scss";
 
 interface SimpleProps {}
 
 const Simple: FC<SimpleProps> = () => (
-  <div className={styles.Simple} data-testid="Simple">
+  <div className={`${styles.Simple} ${page_styles.Page}`} data-testid="Simple">
     Simple Component
   </div>
 );
