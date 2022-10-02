b337fb43c9a376659999493a78f4f7b4e4fa05ae
JohnRamberger <jramberger3@gatech.edu>
2022-10-01 21:48:13 -0400

    Updated Simple->Navbar
    added styling

--- src/components/Simple/Navbar/Navbar.tsx
+++ src/components/Simple/Navbar/Navbar.tsx
@@ -3,14 +3,42 @@ import styles from "./Navbar.module.scss";
 
 import Flex from "../../../layout/Flex/Flex";
 
+//load content
+import { NavbarContent as cont } from "../../../config/Content";
+import { Link } from "react-router-dom";
+
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => (
   <div className={styles.Navbar} data-testid="Navbar">
-    <Flex>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
+    <Flex
+      dir="row"
+      style={{
+        height: "100%",
+        width: "100%",
+        alignItems: "center",
+        gap: "1em",
+        padding: "0 1em",
+      }}
+    >
+      <h1 className={styles.Major}>{cont.title}</h1>
+      <Flex
+        dir="row"
+        style={{
+          height: "100%",
+          justifyContent: "flex-end",
+          alignItems: "stretch",
+          flexGrow: 1,
+          gap: "1em",
+          border: "1px solid blue",
+        }}
+      >
+        {cont.links.map((l, i) => (
+          <Link key={i} to={l.url} className={styles.Link}>
+            {l.label}
+          </Link>
+        ))}
+      </Flex>
     </Flex>
   </div>
 );
