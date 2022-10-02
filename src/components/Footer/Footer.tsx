69187d499f15c68124cb81da1a007731b2444f3d
JohnRamberger <jramberger3@gatech.edu>
2022-10-01 22:21:18 -0400

    Updated Footer
    added content and styling, set up wrapper

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,29 @@
-import React, { FC } from 'react';
-import styles from './Footer.module.scss';
+import React, { FC } from "react";
+import styles from "./Footer.module.scss";
+
+import Flex from "../../layout/Flex/Flex";
+
+import { FooterContent as cont } from "../../config/Content";
+import config from "../../config/config";
 
 interface FooterProps {}
 
 const Footer: FC<FooterProps> = () => (
   <div className={styles.Footer} data-testid="Footer">
-    Footer Component
+    <div
+      className={styles.Wrapper}
+      style={{ maxWidth: config.wrapper.default }}
+    >
+      <Flex dir="row" style={{ gap: "1em", flexWrap: "wrap" }}>
+        {cont.sections.map((s, i) => {
+          return (
+            <Flex key={i} dir="column" style={{ flexWrap: "wrap" }}>
+              {s.label}
+            </Flex>
+          );
+        })}
+      </Flex>
+    </div>
   </div>
 );
 
