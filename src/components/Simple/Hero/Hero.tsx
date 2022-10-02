7fa4096a03c143883850acc4331436c3c07d2dfd
JohnRamberger <jramberger3@gatech.edu>
2022-10-01 21:56:45 -0400

    Update Hero.tsx
    added content

--- src/components/Simple/Hero/Hero.tsx
+++ src/components/Simple/Hero/Hero.tsx
@@ -1,11 +1,14 @@
-import React, { FC } from 'react';
-import styles from './Hero.module.scss';
+import React, { FC } from "react";
+import styles from "./Hero.module.scss";
+
+import { HeroContent } from "../../../config/Content";
 
 interface HeroProps {}
 
 const Hero: FC<HeroProps> = () => (
   <div className={styles.Hero} data-testid="Hero">
-    Hero Component
+    <h1>{HeroContent.major}</h1>
+    <h2>{HeroContent.minor}</h2>
   </div>
 );
 
