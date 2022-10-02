88ee72dde055909254cbe1358dc0f8d22272289f
JohnRamberger <jramberger3@gatech.edu>
2022-10-01 23:57:08 -0400

    Updated Hero
    Styled, made button optional

--- src/components/Simple/Hero/Hero.tsx
+++ src/components/Simple/Hero/Hero.tsx
@@ -4,17 +4,35 @@ import styles from "./Hero.module.scss";
 import { HeroContent as cont } from "../../../config/Content";
 import { Link } from "react-router-dom";
 import Section from "../../../layout/Section/Section";
+import Flex from "../../../layout/Flex/Flex";
 
 interface HeroProps {}
 
 const Hero: FC<HeroProps> = () => (
   <div className={styles.Hero} data-testid="Hero">
     <Section>
-      <h1>{cont.major}</h1>
-      <h2>{cont.minor}</h2>
-      <Link to={cont.button.url} className={styles.Button}>
-        {cont.button.label}
-      </Link>
+      <Flex
+        dir="row"
+        style={{
+          width: "100%",
+          padding: "2em 0",
+          minHeight: "300px",
+          // justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Flex dir="column" style={{ gap: "0.5em" }}>
+          <h1 className={styles.Major}>{cont.major}</h1>
+          <h2 className={styles.Minor}>{cont.minor}</h2>
+          {cont.button ? (
+            <Link to={cont.button.url} className={styles.Button}>
+              {cont.button.label}
+            </Link>
+          ) : (
+            ""
+          )}
+        </Flex>
+      </Flex>
     </Section>
   </div>
 );
