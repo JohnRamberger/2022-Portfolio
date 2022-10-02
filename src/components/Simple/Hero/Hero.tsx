3a98074e30182bb1338999a12c3dab6ca7fc3763
JohnRamberger <jramberger3@gatech.edu>
2022-10-01 22:01:38 -0400

    Update Hero.tsx
    added button

--- src/components/Simple/Hero/Hero.tsx
+++ src/components/Simple/Hero/Hero.tsx
@@ -1,14 +1,16 @@
 import React, { FC } from "react";
 import styles from "./Hero.module.scss";
 
-import { HeroContent } from "../../../config/Content";
+import { HeroContent as cont } from "../../../config/Content";
+import { Link } from "react-router-dom";
 
 interface HeroProps {}
 
 const Hero: FC<HeroProps> = () => (
   <div className={styles.Hero} data-testid="Hero">
-    <h1>{HeroContent.major}</h1>
-    <h2>{HeroContent.minor}</h2>
+    <h1>{cont.major}</h1>
+    <h2>{cont.minor}</h2>
+    <Link to={cont.button.url}>{cont.button.label}</Link>
   </div>
 );
 
