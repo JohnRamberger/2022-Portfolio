b5f28731284473a518747aa3c926a44f228432c2
JohnRamberger <jramberger3@gatech.edu>
2022-10-01 23:24:55 -0400

    Update Hero.tsx
    wrapped in section

--- src/components/Simple/Hero/Hero.tsx
+++ src/components/Simple/Hero/Hero.tsx
@@ -3,14 +3,19 @@ import styles from "./Hero.module.scss";
 
 import { HeroContent as cont } from "../../../config/Content";
 import { Link } from "react-router-dom";
+import Section from "../../../layout/Section/Section";
 
 interface HeroProps {}
 
 const Hero: FC<HeroProps> = () => (
   <div className={styles.Hero} data-testid="Hero">
-    <h1>{cont.major}</h1>
-    <h2>{cont.minor}</h2>
-    <Link to={cont.button.url} className={styles.Button}>{cont.button.label}</Link>
+    <Section>
+      <h1>{cont.major}</h1>
+      <h2>{cont.minor}</h2>
+      <Link to={cont.button.url} className={styles.Button}>
+        {cont.button.label}
+      </Link>
+    </Section>
   </div>
 );
 
