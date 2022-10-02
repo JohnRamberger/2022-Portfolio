b0c6db5d8df8b4ea0cae2f11dbbb9e6766cb42b3
JohnRamberger <jramberger3@gatech.edu>
2022-10-01 22:08:45 -0400

    Updated Hero
    added button styling

--- src/components/Simple/Hero/Hero.tsx
+++ src/components/Simple/Hero/Hero.tsx
@@ -10,7 +10,7 @@ const Hero: FC<HeroProps> = () => (
   <div className={styles.Hero} data-testid="Hero">
     <h1>{cont.major}</h1>
     <h2>{cont.minor}</h2>
-    <Link to={cont.button.url}>{cont.button.label}</Link>
+    <Link to={cont.button.url} className={styles.Button}>{cont.button.label}</Link>
   </div>
 );
 
