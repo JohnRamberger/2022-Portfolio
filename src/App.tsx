2045df3d46bc46849ca4ca931f21ec250a516be1
JohnRamberger <jramberger3@gatech.edu>
2022-10-01 22:30:17 -0400

    Update App.tsx
    now checks for changes in theme param

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./App.module.scss";
 //router
 import { useParams } from "react-router-dom";
@@ -12,13 +12,25 @@ import ThemeMap from "./config/ThemeMap";
 function App() {
   const { theme } = useParams();
   // Load the site theme based on the param, or load the default theme if not found
-  const [currentTheme] = useState<number>(
+  const [currentTheme, setCurrentTheme] = useState<number>(
     theme && ThemeMap.has(parseInt(theme))
       ? parseInt(theme)
       : config.randomTheme
       ? Math.floor(Math.random() * Array.from(ThemeMap.keys()).length)
       : config.defaultTheme
   );
+
+  useEffect(() => {
+    let newValue =
+      theme && ThemeMap.has(parseInt(theme))
+        ? parseInt(theme)
+        : config.randomTheme
+        ? Math.floor(Math.random() * Array.from(ThemeMap.keys()).length)
+        : config.defaultTheme;
+    if (newValue !== currentTheme) {
+      setCurrentTheme(newValue);
+    }
+  }, [theme]);
 
   return <div className={styles.App}>{ThemeMap.get(currentTheme)}</div>;
 }
