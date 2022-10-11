40f0297b7e018f5ba8257c406085320cfd6693ef
JohnRamberger <jramberger3@gatech.edu>
2022-10-11 18:49:15 -0400

    Updated ProjectCard

--- src/components/Simple/ProjectCard/ProjectCard.tsx
+++ src/components/Simple/ProjectCard/ProjectCard.tsx
@@ -1,11 +1,21 @@
-import React, { FC } from 'react';
-import styles from './ProjectCard.module.scss';
+import React, { FC } from "react";
+import styles from "./ProjectCard.module.scss";
 
-interface ProjectCardProps {}
+import { Project } from "../../../config/Content";
+import Flex from "../../../layout/Flex/Flex";
 
-const ProjectCard: FC<ProjectCardProps> = () => (
+interface ProjectCardProps {
+  project?: Project;
+}
+
+const ProjectCard: FC<ProjectCardProps> = ({ project }) => (
   <div className={styles.ProjectCard} data-testid="ProjectCard">
-    ProjectCard Component
+    <Flex dir="column" style={{ width: "100%" }}>
+      <div className={styles.Image}>
+        <img alt={project?.name} src={project?.image} />
+      </div>
+      <div className={styles.Title}>{`${project?.name} ${project?.type}`}</div>
+    </Flex>
   </div>
 );
 
