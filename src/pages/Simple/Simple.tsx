f39e9cf9ab9fa69293dc7abd61020297dfda8456
JohnRamberger <jramberger3@gatech.edu>
2022-10-11 17:40:01 -0400

    Update Simple.tsx
    Added projects template

--- src/pages/Simple/Simple.tsx
+++ src/pages/Simple/Simple.tsx
@@ -5,6 +5,9 @@ import page_styles from "../page.module.scss";
 import Navbar from "../../components/Simple/Navbar/Navbar";
 import Hero from "../../components/Simple/Hero/Hero";
 import Footer from "../../components/Footer/Footer";
+import Section from "../../layout/Section/Section";
+import Flex from "../../layout/Flex/Flex";
+import ProjectCard from "../../components/Simple/ProjectCard/ProjectCard";
 
 interface SimpleProps {}
 
@@ -12,6 +15,13 @@ const Simple: FC<SimpleProps> = () => (
   <div className={`${styles.Simple} ${page_styles.Page}`} data-testid="Simple">
     <Navbar />
     <Hero />
+    <Section>
+      <Flex dir="row" style={{ gap: "1em" }}>
+        <ProjectCard></ProjectCard>
+        <ProjectCard></ProjectCard>
+        <ProjectCard></ProjectCard>
+      </Flex>
+    </Section>
     <Footer />
   </div>
 );
