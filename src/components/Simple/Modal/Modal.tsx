d2071744d902b59445539aa0bade9c56fde7a24d
JohnRamberger <jramberger3@gatech.edu>
2022-10-12 14:34:50 -0400

    Updated Modal
    setup modal

--- src/components/Simple/Modal/Modal.tsx
+++ src/components/Simple/Modal/Modal.tsx
@@ -1,12 +1,50 @@
-import React, { FC } from 'react';
-import styles from './Modal.module.scss';
+import React, { Dispatch, FC, SetStateAction } from "react";
+import styles from "./Modal.module.scss";
 
-interface ModalProps {}
+import close_icon from "../../../assets/icons/cross-small-skinny.svg";
 
-const Modal: FC<ModalProps> = () => (
-  <div className={styles.Modal} data-testid="Modal">
-    Modal Component
-  </div>
-);
+interface ModalProps {
+  open?: boolean;
+  children?: React.ReactNode;
+  setOpen?: Dispatch<SetStateAction<boolean>>;
+  setConfirmed?: Dispatch<SetStateAction<boolean>>;
+  handleConfirmed?: () => void;
+}
+
+const Modal: FC<ModalProps> = ({
+  open,
+  children,
+  setOpen,
+  setConfirmed,
+  handleConfirmed,
+}) => {
+  const closeModal = () => {
+    if (setOpen !== undefined) setOpen(false);
+  };
+  const handleConfirm = async () => {
+    if (setConfirmed !== undefined) {
+      setConfirmed(true);
+      closeModal();
+    }
+    if (handleConfirmed !== undefined) {
+      handleConfirmed();
+      closeModal();
+    }
+  };
+  return (
+    <div className={styles.Modal} data-testid="Modal">
+      <div className={styles.bg} onClick={closeModal}></div>
+      <div className={styles.Menu}>
+        <img
+          className={styles.MenuClose}
+          src={close_icon}
+          alt="Close Icon"
+          onClick={closeModal}
+        />
+        {children}
+      </div>
+    </div>
+  );
+};
 
 export default Modal;
