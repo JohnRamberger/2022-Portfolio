0d63ff62033e15f115847e449bb0fbab77131b42
JohnRamberger <jramberger3@gatech.edu>
2022-10-13 10:18:44 -0400

    Update ExperienceCard.tsx
    added event groupings

--- src/components/Simple/ExperienceCard/ExperienceCard.tsx
+++ src/components/Simple/ExperienceCard/ExperienceCard.tsx
@@ -5,21 +5,23 @@ import { ExperienceContent, Experience } from "../../../config/Content";
 import Flex from "../../../layout/Flex/Flex";
 
 interface ExperienceCardProps {
-  experience?: Experience;
+  experience?: Experience[];
 }
 
 const ExperienceCard: FC<ExperienceCardProps> = ({ experience }) => (
   <div className={styles.ExperienceCard} data-testid="ExperienceCard">
     <Flex dir="row" style={{ gap: "1em" }}>
       <div className={styles.Image}>
-        <img
-          src={experience?.company_image}
-          alt={experience?.company + " logo"}
-        />
+        {experience && experience[0] ? (
+          <img
+            src={experience[0]?.company_image}
+            alt={experience[0]?.company + " logo"}
+          />
+        ) : (
+          ""
+        )}
       </div>
-      <Flex dir="column" style={{gap: "0.5em"}}>
-        
-      </Flex>
+      <Flex dir="column" style={{ gap: "0.5em" }}></Flex>
     </Flex>
   </div>
 );
