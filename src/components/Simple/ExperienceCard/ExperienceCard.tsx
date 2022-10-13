94033b3a80758e5eb0d303cc2f2cb0ab22700568
JohnRamberger <jramberger3@gatech.edu>
2022-10-13 10:27:17 -0400

    Update ExperienceCard.tsx
    added elements for multiple experiences provided for company

--- src/components/Simple/ExperienceCard/ExperienceCard.tsx
+++ src/components/Simple/ExperienceCard/ExperienceCard.tsx
@@ -8,22 +8,34 @@ interface ExperienceCardProps {
   experience?: Experience[];
 }
 
-const ExperienceCard: FC<ExperienceCardProps> = ({ experience }) => (
-  <div className={styles.ExperienceCard} data-testid="ExperienceCard">
-    <Flex dir="row" style={{ gap: "1em" }}>
-      <div className={styles.Image}>
-        {experience && experience[0] ? (
-          <img
-            src={experience[0]?.company_image}
-            alt={experience[0]?.company + " logo"}
-          />
+const ExperienceCard: FC<ExperienceCardProps> = ({ experience }) => {
+  return (
+    <div className={styles.ExperienceCard} data-testid="ExperienceCard">
+      <Flex dir="row" style={{ gap: "1em" }}>
+        <div className={styles.Image}>
+          {experience && experience[0] ? (
+            <img
+              src={experience[0]?.company_image}
+              alt={experience[0]?.company + " logo"}
+            />
+          ) : (
+            ""
+          )}
+        </div>
+        {experience && experience.length > 1 ? (
+          //greater than 1 position
+          <Flex dir="column" style={{ gap: "0.5em" }}>
+            a
+          </Flex>
         ) : (
-          ""
+          // only 1 position
+          <Flex dir="column" style={{ gap: "0.5em" }}>
+            b
+          </Flex>
         )}
-      </div>
-      <Flex dir="column" style={{ gap: "0.5em" }}></Flex>
-    </Flex>
-  </div>
-);
+      </Flex>
+    </div>
+  );
+};
 
 export default ExperienceCard;
