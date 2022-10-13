a5ffd9198f2eeeed26cab9fec164019c62ce7a73
JohnRamberger <jramberger3@gatech.edu>
2022-10-13 09:49:43 -0400

    Updated ExperienceCard
    added styling and elements

--- src/components/Simple/ExperienceCard/ExperienceCard.tsx
+++ src/components/Simple/ExperienceCard/ExperienceCard.tsx
@@ -10,13 +10,16 @@ interface ExperienceCardProps {
 
 const ExperienceCard: FC<ExperienceCardProps> = ({ experience }) => (
   <div className={styles.ExperienceCard} data-testid="ExperienceCard">
-    <Flex dir="row">
+    <Flex dir="row" style={{ gap: "1em" }}>
       <div className={styles.Image}>
         <img
           src={experience?.company_image}
           alt={experience?.company + " logo"}
         />
       </div>
+      <Flex dir="column" style={{gap: "0.5em"}}>
+        
+      </Flex>
     </Flex>
   </div>
 );
