cf71208ff7ad06bc21b65b4154bfe7fe3d4deb70
JohnRamberger <jramberger3@gatech.edu>
2022-12-09 15:41:23 -0500

    Update ThemeMap.tsx

--- src/config/ThemeMap.tsx
+++ src/config/ThemeMap.tsx
@@ -1,9 +1,11 @@
+import Retro from "../pages/Retro/Retro";
 import SciFi from "../pages/SciFi/SciFi";
 import Simple from "../pages/Simple/Simple";
 
 const ThemeMap = new Map<number, JSX.Element>([
   [0, <Simple />],
   [1, <SciFi />],
+  [2, <Retro />],
 ]);
 
 export default ThemeMap;
